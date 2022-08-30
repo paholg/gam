a37ef455bcf76c2a76647802f47a5bc8a249871b
Paho Lurie-Gregg <paho@paholg.com>
2022-08-29 17:10:33 -0700

    Movable sphere on a plane

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+nix-shell --run "cargo run --features bevy/dynamic"
---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs; mkShell {
+  nativeBuildInputs = [
+    pkgconfig
+    clang lld # To use lld linker
+  ];
+  buildInputs = [
+    udev alsaLib vulkan-loader
+    xlibsWrapper xorg.libXcursor xorg.libXrandr xorg.libXi # To use x11 feature
+  ];
+  shellHook = ''export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
+    udev alsaLib vulkan-loader
+  ]}"'';
+}
