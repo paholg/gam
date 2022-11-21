108de202374794fe15b6d419a489206612cebec4
Paho Lurie-Gregg <paho@paholg.com>
2022-11-20 17:16:06 -0800

    Switch to FixedTimestep for most systems; flawed
    We switch all systems except Ai to use fixed timestep. Ai still needs to
    migrate.
    
    It's not working properly. We still tick once per frame instead of once
    per TIMESTEP, as can be seen with the TickDebugPlugin.

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,28 @@
-use std::time::Duration;
-
 use bevy::{
-    prelude::{App, Vec3},
+    prelude::{default, App, PluginGroup},
+    window::{PresentMode, WindowDescriptor, WindowPlugin},
     DefaultPlugins,
 };
-use bevy_rapier3d::prelude::{NoUserData, RapierConfiguration, RapierPhysicsPlugin};
-use gam::{ability, ai, healthbar::HealthbarPlugin, player_cooldown_system, system, NumAi};
-use iyes_loopless::prelude::AppLooplessFixedTimestepExt;
-
-const TIMESTEP: Duration = Duration::from_millis(17);
+use gam::{
+    time::TickDebugPlugin, GamClientPlugin, GamPlugin,
+};
 
 fn main() {
-    let mut rapier_config = RapierConfiguration::default();
-    rapier_config.gravity = Vec3::ZERO;
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_fixed_timestep(TIMESTEP, "timestep")
-        .insert_resource(NumAi {
-            enemies: 1,
-            allies: 1,
-        })
-        .add_startup_system(gam::setup)
-        .add_system(system::player_input)
-        .add_system(system::update_cursor)
-        .add_system(system::die)
-        .add_system(system::reset)
-        .add_system(ability::hyper_sprint_system)
-        .add_system(ability::shot_despawn_system)
-        .add_system(ability::shot_hit_system)
-        .add_system(ability::shot_miss_system)
-        .add_system(player_cooldown_system)
-        .add_plugin(HealthbarPlugin)
-        .add_plugin(ai::simple::SimpleAiPlugin)
-        .add_plugin(ai::qlearning::QLearningPlugin)
-        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
-        .insert_resource(rapier_config)
+        // .add_plugins(HeadlessDefaultPlugins)
+        // .add_plugins(ClientDefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                present_mode: PresentMode::AutoNoVsync,
+                ..default()
+            },
+            ..default()
+        }))
+        .add_plugin(GamPlugin)
+        .add_plugin(GamClientPlugin)
+        .add_plugin(TickDebugPlugin)
+        // .add_plugin(bevy::diagnostic::LogDiagnosticsPlugin::default())
+        // .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin::default())
         // .add_plugin(bevy_rapier3d::render::RapierDebugRenderPlugin::default())
-        // .add_plugin(FrameTimeDiagnosticsPlugin::default())
-        // .add_plugin(LogDiagnosticsPlugin::default())
         .run();
 }
