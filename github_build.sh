07baa9dd8318b2e71721d290b3eb852db5cc46b2
Paho Lurie-Gregg <paho@paholg.com>
2023-11-21 02:54:37 -0800

    Setup release without cross-compiling

--- github_build.sh
+++ github_build.sh
@@ -2,22 +2,13 @@
 
 set -euo pipefail
 
-# For Linux:
-sudo apt-get update
-sudo apt-get install --no-install-recommends libasound2-dev libudev-dev
+target="$1"
 
-# For Windows:
-sudo apt-get install mingw-w64
+echo "Building for $target"
+cargo build --release --locked --target "$target" --bin client
 
-targets=("x86_64-unknown-linux-gnu" "x86_86-pc-windows-gnu")
-
-for target in "${targets[@]}"; do
-  echo "Building for $target"
-  cargo build --release --locked --target "$target" --bin client
-
-  mkdir "gam"
-  cp "target/$target/release/client" "gam/"
-  cp -r assets/ "gam/"
-  7z a -tzip "gam-$target.zip" "gam/"
-  rm -r "gam"
-done
+mkdir "gam"
+cp "target/$target/release/client" "gam/"
+cp -r assets/ "gam/"
+7z a -tzip "gam-$target.zip" "gam/"
+rm -r "gam"
